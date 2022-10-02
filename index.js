32e160d2cae2e5938147838710fb7e531a574ae3
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-10-02 23:11:30 +0530

    cors commit

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ dotenv.config();
 
 // middlewares
 app.use(express.json());
-app.use(cors({
-    origin: "https://whatssup.netlify.app"
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');

a1df8786de1f5e07a21086be205508af157c211d
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-10-02 15:33:25 +0530

    cors origin

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ dotenv.config();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://whatssup.netlify.app"
+}));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');

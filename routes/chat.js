53761791a089a709278b77b8abf2642626b43dfe
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-10-09 06:45:58 +0530

    commit

--- routes/chat.js
+++ routes/chat.js
@@ -7,29 +7,36 @@ import mongoose from "mongoose";
 // create new chat
 router.post("/create-chat", async (req, res) => {
     try {
+        // find contact is registered or not
         const findMobileNumber = await User.findOne({ contact: req.body.contact });
 
         if (findMobileNumber) {
-
             const friendId = findMobileNumber._id.toString();
 
-            const findChat = await Chat.findOne({ members: { $in: [req.body.userId, friendId] } });
-            if (findChat) {
-                const updateUserId = await User.updateOne({ _id: req.body.userId }, { $push: { chats: findChat._id.toString() } });
-                res.send({ msg: `Chat created with ${req.body.name}`, data: findChat });
+            //  find chat of user with friend
+            const findChat = await Chat.findOne({ members: { $all: [req.body.userId, friendId] } }).populate("members", { _id: 1, name: 1, contact: 1 });
+
+            // // check if chat already exists in user's chats array
+            const findUserData = await User.findOne({ _id: req.body.userId });
+            if (findUserData.chats.includes(findChat?._id)) {
+                res.send({ msg: "Contact already exists", data: findChat });
+
             } else {
-                const newChat = await Chat.create({
-                    members: [req.body.userId, friendId],
-                    latestMessage: null
-                });
-                const updateUserId = await User.updateOne({ _id: req.body.userId }, { $push: { chats: newChat._id.toString() } });
-                // res.send({ msg: `Chat created with ${req.body.name}` });
-                // res.send(newChat);
-                res.send({ msg: `Chat created with ${req.body.name}`, data: newChat });
+                if (findChat) {
+                    const updateUserId = await User.updateOne({ _id: req.body.userId }, { $push: { chats: findChat._id.toString() } });
+                    res.send({ msg: `Chat created with ${req.body.name}`, data: findChat });
+                } else {
+                    const newChat = await Chat.create({
+                        members: [req.body.userId, friendId],
+                    });
+                    const updateUserId = await User.updateOne({ _id: req.body.userId }, { $push: { chats: newChat._id.toString() } });
+                    res.send({ msg: `New Chat created with ${req.body.name}`, data: newChat });
+                }
             }
-            // console.log(findMobileNumber._id.toString());
+
+
         } else {
-            res.send({ msg: "User not registered with provided mobile number" })
+            res.send({ msg: "User not registered" })
         }
     } catch (error) {
         res.send({ msg: "failed to create chat", error: error.message })
@@ -39,7 +46,7 @@ router.post("/create-chat", async (req, res) => {
 //  all chats
 router.get("/get-chat", async (req, res) => {
     try {
-        const chats = await Chat.find({}).populate("members", { password: 0, contactList: 0 });
+        const chats = await Chat.find({}).populate("members", { _id: 1, name: 1, contact: 1 });
         res.send(chats)
     } catch (error) {
         res.send("failed to retrieve chat")
@@ -49,8 +56,8 @@ router.get("/get-chat", async (req, res) => {
 // user's chat
 router.get("/get-chat/:userId", async (req, res) => {
     try {
-        // const chats = await Chat.find({ members: { $in: [req.params.userId] } }).populate("members", { password: 0, contactList: 0 });
-        const chats = [];
+        const chats = await Chat.find({ members: { $in: [req.params.userId] } }).populate("members", { _id: 1, name: 1, contact: 1 });
+        // const chats = [];
         res.send(chats)
     } catch (error) {
         res.send("failed to retrieve chat")
